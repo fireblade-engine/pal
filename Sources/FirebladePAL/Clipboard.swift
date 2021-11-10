b13b0560454822f3c9b47948565cb6971881cd73
Christian Treffs <ctreffs@gmail.com>
2021-11-10 06:40:07 +0100

    Add Clipboard protocol

---
+++ Sources/FirebladePAL/Clipboard.swift
@@ -0,0 +1,17 @@
+//
+// Clipboard.swift
+// Fireblade PAL
+//
+// Copyright © 2018-2021 Fireblade Team. All rights reserved.
+// Licensed under MIT License. See LICENSE file for details.
+
+public protocol Clipboard {
+    /// Get UTF-8 text from the clipboard.
+    func getText() throws -> String
+
+    /// Put UTF-8 text into the clipboard.
+    func setText(_ text: String) throws
+
+    /// Query whether the clipboard exists and contains a non-empty text string.
+    func hasText() -> Bool
+}
