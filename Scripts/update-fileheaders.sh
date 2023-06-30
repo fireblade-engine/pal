ce63f541d438b6d24ffb707253b362c45762cc4f
Christian Treffs <ctreffs@gmail.com>
2023-06-30 09:16:22 +0200

    Move file header script to make file

--- Scripts/update-fileheaders.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SOURCES_DIR="${PWD}/Sources"
-FILEHEADER="\n{file}\nFireblade PAL\n\nCopyright © 2018-{year} Fireblade Team. All rights reserved.\nLicensed under MIT License. See LICENSE file for details."
-
-# Format header
-mint run swiftformat $SOURCES_DIR --exclude **/*.generated.swift --header "$FILEHEADER" --swiftversion `swift package tools-version`
